992ea6caeaf7a4f3b27429d6ab0865331e61b11b
puichingbill <puichingbill@proton.me>
2022-07-13 11:43:55 +0000

    Various minor fixes

--- cpp_image_processing/cv_test.cpp
+++ cpp_image_processing/cv_test.cpp
@@ -10,4 +10,9 @@ int main()
 {
     clock_t start_time, end_time;
     start_time = clock();
-    cv::Mat image_a, imag
+    cv::Mat image_a, image_b, image_c;
+
+    image_a = cv::imread("IMG_1446.JPG", 1);
+    if ( !image_a.data )
+    {
+        cout << "No image data 
